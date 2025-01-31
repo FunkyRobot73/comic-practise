a72e824f4931992f6fd11e44706d901a21f57ce6
unknown <howtobeastupid@gmail.com>
2025-01-31 00:03:41 -0500

    adding the add-comic component

---
+++ src/app/comics/add-comic/add-comic.component.ts
@@ -0,0 +1,14 @@
+import { Component, Inject, inject, Input, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ComicbookService } from '../../services/comicbook.service';
+
+@Component({
+  selector: 'app-add-comic',
+  imports: [],
+  templateUrl: './add-comic.component.html',
+  styleUrl: './add-comic.component.css'
+})
+export class AddComicComponent {
+
+}
