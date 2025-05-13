97a8ff23d6b04ba007050a2973ce77d50c0a293a
unknown <howtobeastupid@gmail.com>
2025-05-13 09:52:48 -0400

    updates on stocks

---
+++ src/app/services/stock.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StockService {
+  private apiUrl = 'https://back.swap2go.ca';
+
+  constructor(private http: HttpClient) { }
+
+  getStocks(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/stock`);
+  }
+
+  addStock(stock: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/addstock`, stock);
+  }
+}
--- src/app/stocks/stocks/stocks.component.ts
+++ src/app/stocks/stocks/stocks.component.ts
@@ -3,6 +3,7 @@ import { ExchangeRateService } from '../../services/exchange-rate.service';
 import { Stockholding } from '../../models/stockholding';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { StockService } from '../../services/stock.service';
 
 @Component({
   selector: 'app-stocks',
@@ -12,34 +13,69 @@ import { CommonModule } from '@angular/common';
 })
 export class StocksComponent implements OnInit {
 
-  holdings: Stockholding[] = [];
-  newHolding: Stockholding = {
-    symbol: '',
-    shares: 0,
-    currency: 'USD',
-    purchaseDate: new Date(),
-    status: 'current'
-  };
+  
   usdToCadRate: number = 1;
 
-  constructor(private exchangeService: ExchangeRateService) {}
+  stocks: any[] = [];
 
-  ngOnInit() {
+  newStock: any = {
+    symbol: '',
+    name: '',
+    date_purchased: '',
+    price_bought_CAD: null,
+    price_sold_CAD: null,
+    price_bought_US: null,
+    price_sold_US: null,
+    date_sold: null,
+    CAN_US_Rate_bought: null,
+    US_CAN_Rate_bought: null,
+    CAN_US_Rate_sold: null,
+    US_CAN_Rate_sold: null,
+    amount: null,
+    notes: ''
+  };
+
+  constructor(private exchangeService: ExchangeRateService, private stockService: StockService) { }
+
+  ngOnInit(): void {
+    this.loadStocks();
     this.exchangeService.getUsdToCadRate().subscribe(rate => {
       this.usdToCadRate = rate;
     });
   }
 
-  addHolding() {
-    this.holdings.push({ ...this.newHolding });
-    this.newHolding = {
+  loadStocks(): void {
+    this.stockService.getStocks().subscribe(
+      (data) => this.stocks = data,
+      (error) => console.error('Error fetching stocks', error)
+    );
+  }
+
+  addStock(): void {
+    this.stockService.addStock(this.newStock).subscribe(
+      (response) => {
+        console.log('Stock added', response);
+        this.loadStocks();
+        this.resetForm();
+      },
+      (error) => console.error('Error adding stock', error)
+    );
+  }
+
+  resetForm(): void {
+    this.newStock = {
       symbol: '',
-      shares: 0,
-      currency: 'USD',
-      purchaseDate: new Date(),
-      status: 'current'
+      name: '',
+      date_purchased: '',
+      price_bought_US: null,
+      CAN_US_Rate_bought: null,
+      US_CAN_Rate_bought: null,
+      notes: '',
+      amount:''
     };
   }
+
+  
 
   getHoldingValueInCAD(holding: Stockholding): number {
     // Dummy price per share for demonstration (replace with real data if needed)
